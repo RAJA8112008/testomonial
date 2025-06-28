7a8144da3bdd3f6ae349de2f9d38bf13f2fe0a14
Raja <kraj9380286@gmail.com>
2025-06-28 23:08:49 +0530

    commited

---
+++ src/components/Card.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import reviews from '../data'
+import {FaQuoteLeft,FaQuoteRight,FaGem} from "react-icons/fa";
+const Card = (props) => {
+    let review=props.review;
+  return (
+    <div className='flex flex-col md:relative'>
+      <div className='absolute top-[-7rem] z-[10] mx-auto'>
+        <img 
+        className='aspect-square rounded-full w-[140px] h-[140px] z-[25] left-[10px]'
+        src={review.image}/>
+        <div className='w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px] z-[-10]'></div>
+      </div>
+     <div>
+       <p>{review.name}</p>
+    </div>
+
+<div>
+    <p>{review.job}</p>
+ </div>
+ <div>
+    <FaQuoteLeft/>
+ </div>
+<div>
+    {review.text}
+</div>
+<div>
+    <FaQuoteRight/>
+ </div>
+ <div>
+    <button>+</button>
+    <button>*</button>
+ </div>
+ <div>
+    <button>Surprise Me</button>
+    
+ </div>
+ 
+
+    </div>
+  )
+}
+
+export default Card
---
+++ src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Pranay Gupta",
+    job: "Software Engineer",
+    image: "https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed by their professionalism and results.",
+  },
+  {
+    id: 2,
+    name: "Abir Pal",
+    job: "Graphic Designer",
+    image: "https://aeccc.targettechnology.in/static/media/AbirPal.574a09ad7cb325853b29.jpg",
+    text: "I have been working with this company for several years now, and I have always been impressed by their creativity and dedication.",
+  },
+  {
+    id: 3,
+    name: "Soumya Banerjee",
+    job: "Marketing Manager",
+    image: "https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d6029842435080.jpg",
+    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn’t have been possible without this team’s efforts.",
+  },
+  {
+    id: 4,
+    name: "Saikat Mukherjee",
+    job: "Content Strategist",
+    image: "https://aeccc.targettechnology.in/static/media/SaikatMukherjee.033310703edff52d0532.jpg",
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this group.",
+  },
+  {
+    id: 5,
+    name: "Aritra Biswas",
+    job: "Data Analyst",
+    image: "https://avatars.githubusercontent.com/u/93366359?v=4",
+    text: "Their attention to detail and data-driven approach helped us make smart, strategic decisions quickly.",
+  },
+];
+
+export default reviews;
